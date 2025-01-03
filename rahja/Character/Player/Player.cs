3775f851b82617d95941bf179c433b1479c9ac4a
AtlasWongy <atlasyjyj@gmail.com>
2025-01-04 00:01:47 +0800

    Project Init
    Let's go!!!

---
+++ rahja/Character/Player/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using Godot;
+
+namespace Rahja.Character.Player
+{
+	public partial class Player : Node
+	{
+		public override void _Ready()
+		{
+			Console.WriteLine("Ready");
+		}
+	}
+}
+
